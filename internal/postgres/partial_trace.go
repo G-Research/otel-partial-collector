46738c966f0a3a6f26b6f5d39714cd6cb03e0a83
Nikola Jokic <jokicnikola07@gmail.com>
2025-03-17 10:30:55 +0100

    Add skip locked

--- internal/postgres/partial_trace.go
+++ internal/postgres/partial_trace.go
@@ -57,7 +57,7 @@ func (db *DB) ListExpiredTraces(ctx context.Context, timestamp time.Time) ([]*Pa
 	q := `
 SELECT trace_id, span_id, trace FROM partial_traces
 WHERE expires_at < $1
-FOR UPDATE
+FOR UPDATE SKIP LOCKED
 	`
 
 	rows, err := db.Query(ctx, q, timestamp)
