c8d4242d219ffa7870d48ec775ce24825219fc17
Jonathan Giannuzzi <jgiannuzzi@users.noreply.github.com>
2025-05-30 20:44:05 +0100

    Batch the DB calls to improve performance

--- internal/postgres/partial_trace.go
+++ internal/postgres/partial_trace.go
@@ -3,51 +3,101 @@ package postgres
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 )
 
-type PartialTrace struct {
+type PartialTraceKey struct {
 	TraceID string
 	SpanID  string
+}
+
+type PartialTrace struct {
+	PartialTraceKey
 	// Marshaled trace to byte slice
 	Trace     []byte
 	Timestamp time.Time
 	ExpiresAt time.Time
 }
 
-func (db *DB) PutTrace(ctx context.Context, partialTrace *PartialTrace) error {
-	q := `
+func (db *DB) PutTrace(ctx context.Context, trace *PartialTrace) error {
+	return db.PutTraces(ctx, []*PartialTrace{trace})
+}
+
+func (db *DB) PutTraces(ctx context.Context, traces []*PartialTrace) error {
+	if len(traces) == 0 {
+		return nil
+	}
+
+	var (
+		valuePlaceholders []string
+		args              []any
+	)
+
+	for i, pt := range traces {
+		// Generate: ($1, $2, $3, $4, $5), ($6, $7, $8, $9, $10), ...
+		n := i * 5
+		valuePlaceholders = append(valuePlaceholders,
+			fmt.Sprintf("($%d, $%d, $%d, $%d, $%d)", n+1, n+2, n+3, n+4, n+5),
+		)
+
+		args = append(args,
+			pt.TraceID,
+			pt.SpanID,
+			pt.Trace,
+			pt.Timestamp,
+			pt.ExpiresAt,
+		)
+	}
+
+	q := fmt.Sprintf(`
 INSERT INTO partial_traces
 (trace_id, span_id, trace, timestamp, expires_at)
-VALUES
-($1, $2, $3, $4, $5)
+VALUES %s
 ON CONFLICT (trace_id, span_id) DO UPDATE
-SET trace = $3, timestamp = $4, expires_at = $5
-`
+SET trace = EXCLUDED.trace,
+    timestamp = EXCLUDED.timestamp,
+    expires_at = EXCLUDED.expires_at
+`, strings.Join(valuePlaceholders, ", "))
 
-	if _, err := db.Exec(
-		ctx,
-		q,
-		partialTrace.TraceID,
-		partialTrace.SpanID,
-		partialTrace.Trace,
-		partialTrace.Timestamp,
-		partialTrace.ExpiresAt,
-	); err != nil {
-		return fmt.Errorf("failed to insert partial span: %w", err)
+	if _, err := db.Exec(ctx, q, args...); err != nil {
+		return fmt.Errorf("failed to upsert partial spans: %w", err)
 	}
 
 	return nil
 }
 
 func (db *DB) RemoveTrace(ctx context.Context, traceID, spanID string) error {
-	q := `
-DELETE FROM partial_traces
-WHERE trace_id = $1 AND span_id = $2
-	`
+	return db.RemoveTraces(ctx, []PartialTraceKey{{traceID, spanID}})
+}
 
-	if _, err := db.Exec(ctx, q, traceID, spanID); err != nil {
-		return fmt.Errorf("failed to delete partial span: %w", err)
+func (db *DB) RemoveTraces(ctx context.Context, traceKeys []PartialTraceKey) error {
+	if len(traceKeys) == 0 {
+		return nil
+	}
+
+	var (
+		valuePlaceholders []string
+		args              []any
+	)
+
+	for i, key := range traceKeys {
+		n := i * 2
+		valuePlaceholders = append(valuePlaceholders,
+			fmt.Sprintf("($%d, $%d)", n+1, n+2),
+		)
+		args = append(args, key.TraceID, key.SpanID)
+	}
+
+	q := fmt.Sprintf(`
+DELETE FROM partial_traces
+WHERE (trace_id, span_id) IN (
+  VALUES %s
+)
+`, strings.Join(valuePlaceholders, ", "))
+
+	if _, err := db.Exec(ctx, q, args...); err != nil {
+		return fmt.Errorf("failed to delete partial spans: %w", err)
 	}
 
 	return nil
